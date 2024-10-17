2990a32f82e1ceb711cedbb50a4a59235410953d
Echochef <M202276618@hust.edu.cn>
2024-10-18 02:44:37 +0800

    code third commit

--- llm_generate/eval_results.sh
+++ llm_generate/eval_results.sh
@@ -5,9 +5,7 @@ foler_path="root_path"
 data_name="math_SFT_seek_answer_stage1_stage2_stage3_deepseek_test"
 input_path="${foler_path}/${data_name}.jsonl"
 output_path="${foler_path}/${data_name}_benchresult.jsonl"
-echo $input_path
-# input_path="/mnt/wx_feature/home/trenousqiu/codes/paper_version/paper_code/data/eval_data/pot_same_stand_gpt_rewrite_output.jsonl"
-# output_path="/mnt/wx_feature/home/trenousqiu/codes/paper_version/paper_code/data/eval_output/pot_same_stand_gpt_rewrite_output_same.jsonl"
+
 dataset="math"  # 替换为实际的 dataset 名称
 # dataset="college"  # 替换为实际的 dataset 名称
 
